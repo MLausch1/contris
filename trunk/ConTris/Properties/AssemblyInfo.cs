cd174ca40d00287609f7b46e607cac9cd7b2efc8
gut.kevin@gmail.com <gut.kevin@gmail.com@b55770fa-c0f0-9605-5ce0-d44c1d9b6082>
2011-04-03 12:42:45 +0000

    Project Created

---
+++ trunk/ConTris/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConTris")]
+[assembly: AssemblyDescription("A Simple Console Application demonstrating the Tetris Class")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AyrA.ch")]
+[assembly: AssemblyProduct("ConTris")]
+[assembly: AssemblyCopyright("Copyright © AyrA.ch 2011")]
+[assembly: AssemblyTrademark("https://AyrA.ch")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2c1272e3-687a-4fcd-ac98-ba53655c889c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.3.3.7")]
+[assembly: AssemblyFileVersion("1.3.3.7")]
